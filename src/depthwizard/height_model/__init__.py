4c1fa1101a8ca0801cb8a55fcf5ff3c33f2e37ad
Amogh R Balakrishna <amoghr110607@gmail.com>
2026-08-26 02:39:10 +0530

    feat: add height model package

---
+++ src/depthwizard/height_model/__init__.py
@@ -0,0 +1,9 @@
+"""DepthWizard remote-sensing height refinement model."""
+
+from depthwizard.height_model.model import (
+    DepthWizardHeightModel,
+    HeightModelConfig,
+    HeightModelOutput,
+)
+
+__all__ = ["DepthWizardHeightModel", "HeightModelConfig", "HeightModelOutput"]
