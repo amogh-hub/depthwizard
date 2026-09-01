b43cbf2989aa13e15c16af8c200b6962b41b5d43
Amogh R Balakrishna <amoghr110607@gmail.com>
2026-09-01 22:40:25 +0530

    Export terrain-structure research interfaces

--- src/depthwizard/height_model/__init__.py
+++ src/depthwizard/height_model/__init__.py
@@ -1,9 +1,21 @@
-"""DepthWizard remote-sensing height refinement model."""
+"""DepthWizard remote-sensing height models."""
 
 from depthwizard.height_model.model import (
     DepthWizardHeightModel,
     HeightModelConfig,
     HeightModelOutput,
 )
+from depthwizard.height_model.terrain_structure import (
+    TerrainStructureConfig,
+    TerrainStructureModel,
+    TerrainStructureOutput,
+)
 
-__all__ = ["DepthWizardHeightModel", "HeightModelConfig", "HeightModelOutput"]
+__all__ = [
+    "DepthWizardHeightModel",
+    "HeightModelConfig",
+    "HeightModelOutput",
+    "TerrainStructureConfig",
+    "TerrainStructureModel",
+    "TerrainStructureOutput",
+]
